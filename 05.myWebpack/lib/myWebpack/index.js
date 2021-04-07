dec79370cc202f12861af278510fe63b458d4d95
scallop <g275053739@163.com>
2021-04-07 16:25:55 +0800

    20210407 自定义webpack

---
+++ 05.myWebpack/lib/myWebpack/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const babelParser = require('@babel/parser')
+
+function myWebpack(config) {
+    return new Compiler(config)
+}
+
+class Compiler {
+    constructor(options = {}) {
+        this.options = options
+    }
+
+    // 启动webpack打包
+    run() {
+        // 1.读取入口文件内容
+        // 入口文件路径
+        const filePath = this.options.entry
+        const file = fs.readFileSync(filePath, 'utf-8')
+        // 2.将其解析成ast抽象语法树
+        const ast = babelParser.parse(file, {
+            sourceType: 'module'
+        })
+        console.log(ast)
+    }
+}
+
+module.exports = myWebpack;
